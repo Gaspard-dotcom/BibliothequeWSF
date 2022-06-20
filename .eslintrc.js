6cfcfdc4e95f7273b83a6c6fec82f897826ad4bf
Gaspard-dotcom <gaspard.couerbe1@gmail.com>
2022-06-20 16:47:13 +0200

    [bug] correction bug eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "plugins": [
+        "react"
+    ],
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "amd": true, // Enables require() and define() as global variables as per the amd spec.
+        "node": true, // Enables Node.js global variables and Node.js scoping.
+        "jest": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "2015",
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+          }
+    },
+    "rules": {
+        "react/react-in-jsx-scope":"off",
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+    }
+
+}
+
